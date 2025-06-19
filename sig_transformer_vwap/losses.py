694137f23cc3e0babd84362273da6aec596ad2e2
paulbourcereau <paul.bourcereau@icloud.com>
2025-06-19 13:44:33 +0200

    Added afs loss for 3 different impact profiles.

--- sig_transformer_vwap/losses.py
+++ sig_transformer_vwap/losses.py
@@ -29,3 +29,27 @@ def volume_curve_loss(y_true, y_pred):
     volume_curve_diff = volume_curve_achieved - volume_curve_mkt
     loss = ops.mean(ops.square(volume_curve_diff))
     return loss
+
+def impact_block(x, lam=1.0):
+    return 0.5 * lam * ops.square(x)
+
+def impact_exp(x, theta=1.0):
+    return theta * ((1 - x) * ops.log(1 - x + 1e-6) + x)
+
+def impact_power(x, alpha=1.0):
+    return ((alpha + 1) / (alpha + 2)) * ops.power(x, (alpha + 2) / (alpha + 1))
+
+def vwap_afs_loss(y_true, y_pred):
+    y_true = ops.cast(y_true, y_pred.dtype)
+    y_pred = ops.cast(y_pred, y_true.dtype)
+
+    # VWAP Loss
+    pred_v, true_v, true_p = y_pred[..., 0], y_true[..., 0], y_true[..., 1]
+    vwap_achieved = ops.sum(pred_v * true_p, axis=1) / ops.sum(pred_v, axis=1, keepdims=True)
+    vwap_mkt = ops.sum(true_v * true_p, axis=1) / ops.sum(true_v, axis=1, keepdims=True)
+    vwap_diff = vwap_achieved / vwap_mkt - 1.
+    vwap_loss = ops.mean(ops.square(vwap_diff))
+    
+    impact_loss = ops.mean(impact_power(ops.cumsum(pred_v)))
+
+    return vwap_loss + impact_loss
