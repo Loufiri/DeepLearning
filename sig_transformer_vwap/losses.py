4714a318ef3aa1c8d061ad5681354352e4bf0d20
Paul Bourcereau <paul.bourcereau@icloud.com>
2025-07-08 12:24:08 +0200

    Update losses.py

--- sig_transformer_vwap/losses.py
+++ sig_transformer_vwap/losses.py
@@ -36,28 +36,31 @@ def impact_exp(x, theta=1.0):
 def impact_power(x, alpha=1.0):
     return ((alpha + 1) / (alpha + 2)) * ops.power(x, (alpha + 2) / (alpha + 1))
 
-def vwap_afs_loss(y_true, y_pred, lambda_impact=0.01, eta_impact=0.01):
+def vwap_afs_loss(lambda_impact=0.01, eta_impact=0.01):
     """
     Loss combinant VWAP relatif et coût d'impact de marché AFS.
 
     """
-    y_true = ops.cast(y_true, y_pred.dtype)
-    y_pred = ops.cast(y_pred, y_true.dtype)
 
-    pred_v = y_pred[..., 0]       # volume exécuté par le modèle
-    true_v = y_true[..., 0]       # volume de marché
-    true_p = y_true[..., 1]       # prix du marché
-
-    # === VWAP with market impact Loss ===
-    cum_pred_v = ops.cumsum(pred_v, axis=1)  # volume cumulé
-
-    impact_permanent = lambda_impact * cum_pred_v
-    impact_temporaire = eta_impact * pred_v
-
-    exec_price = true_p + impact_permanent + impact_temporaire
-    vwap_exec = ops.sum(exec_price * pred_v, axis=1) / ops.sum(pred_v, axis=1)
-    vwap_mkt = ops.sum(true_v * true_p, axis=1) / ops.sum(true_v, axis=1)
-    vwap_diff = vwap_exec / vwap_mkt - 1.
-    vwap_loss = ops.mean(ops.square(vwap_diff))
-
-    return vwap_loss
+    def loss(y_true, y_pred):
+        y_true = ops.cast(y_true, y_pred.dtype)
+        y_pred = ops.cast(y_pred, y_true.dtype)
+    
+        pred_v = y_pred[..., 0]       # volume exécuté par le modèle
+        true_v = y_true[..., 0]       # volume de marché
+        true_p = y_true[..., 1]       # prix du marché
+    
+        # === VWAP with market impact Loss ===
+        cum_pred_v = ops.cumsum(pred_v, axis=1)  # volume cumulé
+    
+        impact_permanent = lambda_impact * cum_pred_v
+        impact_temporaire = eta_impact * pred_v
+    
+        exec_price = true_p + impact_permanent + impact_temporaire
+        vwap_exec = ops.sum(exec_price * pred_v, axis=1) / ops.sum(pred_v, axis=1)
+        vwap_mkt = ops.sum(true_v * true_p, axis=1) / ops.sum(true_v, axis=1)
+        vwap_diff = vwap_exec / vwap_mkt - 1.
+        vwap_loss = ops.mean(ops.square(vwap_diff))
+    
+        return vwap_loss
+    return loss
